360d7e5518061d05953ad7f57f715f9313d5ca4c
Meho <Meho.Aliefendic@zira.com.ba>
2023-05-22 01:13:46 +0200

    Confired docker compose and check to wait for db

--- app/app/calc.py
+++ app/app/calc.py
@@ -2,9 +2,12 @@
 Clculator functions
 """
 
+
 def add(x, y):
     """Add x and y and return result."""
     return x+y
+
+
 def substract(x, y):
     """Subtract x from y and retunt result"""
     return x - y
