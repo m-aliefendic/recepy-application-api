3af2a92c45fe97e35a7141c77f5a4dbd6ac08cbe
Meho <Meho.Aliefendic@zira.com.ba>
2023-05-23 17:45:40 +0200

    customize admin page

--- app/core/models.py
+++ app/core/models.py
@@ -29,6 +29,7 @@ class UserManager(BaseUserManager):
         user.is_staff= True
         user.is_superuser = True
         user.save(using=self._db)
+        return user
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
