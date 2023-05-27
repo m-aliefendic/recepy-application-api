e4fd222a9e597232b94804c105b9ff18c4abfc08
Meho <Meho.Aliefendic@zira.com.ba>
2023-05-28 00:18:01 +0200

    Add token

--- app/user/views.py
+++ app/user/views.py
@@ -2,6 +2,8 @@
 Views for the user API.
 """
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 from user.serializers import UserSerializer
 
@@ -9,3 +11,8 @@ from user.serializers import UserSerializer
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
     serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
