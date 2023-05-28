1e9c1ac8ed799a60c029b62a0ba2123865301452
Meho <Meho.Aliefendic@zira.com.ba>
2023-05-28 11:22:53 +0200

    Bug fix

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,7 +11,7 @@ from rest_framework import status
 
 
 CREATE_USER_URL = reverse('user:create')
-TOKEN_URL = ('user:token')
+TOKEN_URL = reverse('user:token')
 
 
 def create_user(**params):
@@ -73,11 +73,11 @@ class PublicUserApiTests(TestCase):
             'email': 'test@example.com',
             'password': 'test-user-password123',
         }
-
         create_user(**user_details)
+
         payload = {
             'email': user_details['email'],
-            'passwprd': user_details['password'],
+            'password': user_details['password'],
         }
 
         res = self.client.post(TOKEN_URL, payload)
@@ -85,12 +85,19 @@ class PublicUserApiTests(TestCase):
         self.assertIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
 
-    def test_create_token_bad_credential(self):
+    def test_create_token_bad_credentials(self):
         """Test returns error if credentials invalid."""
         create_user(email='test@example.com', password='goodpass')
         payload = {'email': 'test@example.com','password': 'badpass'}
         res = self.client.post(TOKEN_URL, payload)
 
+        self.assertNotIn('token', res.data)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_create_token_email_not_found(self):
+        """Test error returned if user not found for given email."""
+        payload = {'email': 'test@example.com', 'password': ''}
+        res = self.client.post(TOKEN_URL, payload)
         self.assertNotIn('token', res.data)
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
--- app/user/views.py
+++ app/user/views.py
@@ -5,7 +5,10 @@ from rest_framework import generics
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.settings import api_settings
 
-from user.serializers import UserSerializer
+from user.serializers import (
+    UserSerializer,
+    AuthTokenSerializer,
+)
 
 
 class CreateUserView(generics.CreateAPIView):
