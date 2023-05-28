255545774bc6bedbd8423b3005b723c67a4fa519
Meho <Meho.Aliefendic@zira.com.ba>
2023-05-28 21:02:20 +0200

    added authorization

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -5,6 +5,7 @@ from django.contrib.auth import (
     get_user_model,
     authenticate,
 )
+
 from django.utils.translation import gettext as _
 from rest_framework import serializers
 
@@ -21,10 +22,22 @@ class UserSerializer(serializers.ModelSerializer):
         """Create and return a suser with encripted password"""
         return get_user_model().objects.create_user(**validated_data)
 
+    def update(self, instance, validated_data):
+        """Update and return user."""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
 
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user auth token."""
-    email = serializers.CharField(
+    email = serializers.EmailField()
+    password = serializers.CharField(
         style={'impout_type': 'password'},
         trim_whitespace=False,
     )
@@ -32,15 +45,15 @@ class AuthTokenSerializer(serializers.Serializer):
     def validate(self, attrs):
         """Validate and authenticate the user."""
         email = attrs.get('email')
-        password = attrs.get('passowrd')
+        password = attrs.get('password')
         user = authenticate(
-            request= self.context.get('request'),
+            request=self.context.get('request'),
             username=email,
             password=password,
         )
         if not user:
-              msg = _('Unable to authenticate with provided credentials.')
-              raise serializers.ValidationError(msg, code='authorisation')
+            msg = _('Unable to authenticate with provided credentials.')
+            raise serializers.ValidationError(msg, code='authorisation')
 
         attrs['user'] = user
         return attrs
